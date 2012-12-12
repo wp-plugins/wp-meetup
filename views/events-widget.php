54e8f25235487b3aa65b32bc79dfca1cb5d07262
Nuanced Media <Nuanced Media@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-12 18:33:14 +0000

    Commiting 1.4.5 and 1.4.6 for real this time
    git-svn-id: https://plugins.svn.wordpress.org/wp-meetup/tags/1.4.6@637942 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/events-widget.php
@@ -0,0 +1,17 @@
+<?php
+if (count($events) > 0) {
+    $ul_contents = '';
+    foreach ($events as $event) {
+        $event_adjusted_time = $event->time + $event->utc_offset;
+        $date_display = $this->element("div", "<span class=\"month\">".date('M', $event_adjusted_time)."</span> <span class=\"date\">".date('d', $event_adjusted_time)."</span>", array('style' => "background-color: {$event->group->color}"));
+        $event_post_link = $this->element('a', $event->name, array('href' => get_permalink($event->post->ID)));
+        $ul_contents .= $this->element('li', $date_display . $event_post_link . (($this->groups->count() > 1)?"<br />" . $event->group->name:""), array('id' => 'wp-meetup-event-'.$event->id));
+    }
+    echo $this->element('ul', $ul_contents, array('id' => 'wp-meetup-upcoming-events-list'));
+} else {
+    echo $this->element('p', __('No upcoming events.'));
+}
+if ($this->options->get('show_nm_link')) {
+	echo "<p align=\"right\"><a href=\"http://nuancedmedia.com/\" title=\"Powered by Nuanced Media\"><img width=40 src=\"" . $this->plugin_url . "images/NM_logo_mini.png\"></a></p>";
+}
+?>
